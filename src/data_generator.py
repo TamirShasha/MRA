18818d3aa4e20dc0d9856dd23c8e84ceccde098f
Tamir <tamirshashafb@gmail.com>
2021-07-20 11:00:18 +0300

    init

---
+++ src/data_generator.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def shift_signal(signal, shift=None, dist=None):
+    """
+    Circular Shifts given signal upon given dist
+    """
+    n = len(signal)
+    if shift is None:
+        if dist is None:
+            shift = np.random.randint(n)
+        else:
+            shift = np.random.choice(np.arange(n), p=dist)
+    shifted_signal = np.roll(signal, shift)
+    return shifted_signal
+
+
+def _add_gaussian_noise(signal, sigma):
+    """
+    :param signal: Clean signal
+    :param sigma: Noise STD
+    :return: Noisy signal
+    """
+    noise = np.random.normal(0, sigma, len(signal))
+    return signal + noise
+
+
+def create_mra_data(signal, num_of_samples, sigma, shift_dist=None):
+    """
+    :param signal: Clean signal inside a window of length L
+    :param num_of_samples: The number of signal shifts to generate
+    :param sigma: Noise STD
+    :param shift_dist: shift distribution
+    :return: noisy signals shifted according to given shift distribution
+    """
+    signals = []
+    for i in range(num_of_samples):
+        shifted_signal = shift_signal(signal, dist=shift_dist)
+        noisy_signal = _add_gaussian_noise(shifted_signal, sigma)
+        signals.append(noisy_signal)
+
+    return np.array(signals)
