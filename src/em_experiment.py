a4723787fa9c6e5ed04a6d2ee6cd16b9ea55f070
Tamir <tamirshashafb@gmail.com>
2021-07-24 15:28:16 +0300

    Asdf

--- src/em_experiment.py
+++ src/em_experiment.py
@@ -6,6 +6,7 @@ from src.utils import shift_signal, relative_error, generate_shift_dist
 from src.em_algorithm import EmAlgorithm, EmAlgorithmFFT
 from src.em_un_algorithm import EmUnAlgorithm, EmUnAlgorithmFFT
 
+np.random.seed(500)
 
 def em_experiment(em_alg: bool = True, use_fft: bool = False, debug_plot=False):
     """
@@ -18,9 +19,10 @@ def em_experiment(em_alg: bool = True, use_fft: bool = False, debug_plot=False):
     L = signal.shape[0]
     noise_std = 1
     ss = [3, 4, 5, 6, 7, 8, 9]
+    # ss = [5]
     shift_dists = [generate_shift_dist(s, L) for s in ss]
 
-    times = 2
+    times = 10
     exp_errs = np.zeros_like(ss, dtype=float)
     for i, shift_dist in enumerate(shift_dists):
         for t in range(times):
@@ -31,26 +33,28 @@ def em_experiment(em_alg: bool = True, use_fft: bool = False, debug_plot=False):
             else:
                 em_algo = EmAlgorithm(mra_data, noise_std) if em_alg else EmUnAlgorithm(mra_data, noise_std)
 
-            em_results = em_algo.run(1)
+            em_results = em_algo.run(20)
             signal_est = em_results[-1, 0] if em_alg else em_results[-1]
             err, shift = relative_error(signal_est, signal)
             exp_errs[i] += err
+            print(err)
 
             if debug_plot:
                 errs = []
-                for signal_est, dist_est in em_results:
-                    err, shift = relative_error(signal_est, signal)
-                    errs.append(err)
+                # for signal_est, dist_est in em_results:
+                #     err, shift = relative_error(signal_est, signal)
+                #     errs.append(err)
 
-                shifted_estimated_signal = shift_signal(signal_est, shift)
+                for j in np.arange(10, 50, 10):
+                    signal_est = em_results[j]
+                    shifted_estimated_signal = shift_signal(signal_est, shift)
+                    plt.plot(np.arange(L), signal)
+                    plt.plot(np.arange(L), shifted_estimated_signal)
+                    plt.show()
 
-                plt.plot(np.arange(L), signal)
-                plt.plot(np.arange(L), shifted_estimated_signal)
-                plt.show()
-
-                plt.plot(np.arange(L), shift_dist)
-                plt.plot(np.arange(L), np.roll(em_results[-1, 1], shift))
-                plt.show()
+                # plt.plot(np.arange(L), shift_dist)
+                # plt.plot(np.arange(L), np.roll(em_results[-1, 1], shift))
+                # plt.show()
 
         exp_errs[i] /= times
 
@@ -58,5 +62,5 @@ def em_experiment(em_alg: bool = True, use_fft: bool = False, debug_plot=False):
     plt.show()
 
 
-em_experiment(em_alg=False, use_fft=True)
-em_experiment(em_alg=True)
+em_experiment(em_alg=True, use_fft=True, debug_plot=False)
+# em_experiment(em_alg=True)
--- src/numerical_error_for_em.py
+++ src/numerical_error_for_em.py
@@ -3,24 +3,27 @@ import matplotlib.pyplot as plt
 
 from src.data_generator import classic_signal, create_mra_data
 from src.utils import generate_shift_dist, relative_error
-from src.em_algorithm import EmAlgorithm
+from src.em_algorithm import EmAlgorithm, EmAlgorithmFFT
 
 
 def em_numerical_error():
     signal = classic_signal()
     N = 2000
     L = signal.shape[0]
-    shift_dist = generate_shift_dist(3, L)
+    # shift_dist = generate_shift_dist(3, L)
+    # shift_dist = np.random.random(L)
+    # shift_dist /= np.sum(shift_dist)
+    shift_dist = np.full(L, 1 / L)
 
-    times = 3
-    noise_stds = np.linspace(0.3, 1, 10)
+    times = 30
+    noise_stds = np.exp(np.linspace(-3, 0, 10))
     exp_errs = np.zeros_like(noise_stds, dtype=float)
     for i, noise_std in enumerate(noise_stds):
         for t in range(times):
             print(f'At experiment #{t} for noise_std {noise_std}')
             mra_data = create_mra_data(signal, N, noise_std, shift_dist)
 
-            em_algo = EmAlgorithm(mra_data, noise_std)
+            em_algo = EmAlgorithmFFT(mra_data, noise_std)
             em_results = em_algo.run()
 
             signal_est = em_results[-1, 0]
@@ -31,7 +34,7 @@ def em_numerical_error():
         print(f'Error for noise std {noise_std} is {exp_errs[i]}')
 
     print(exp_errs)
-    plt.plot(np.log(noise_stds), np.log(exp_errs))
+    plt.plot(np.log(noise_stds), np.log(exp_errs), marker='o')
     plt.show()
 
 
